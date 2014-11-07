8312af22be97017572cec281e587c81ae4b28490
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2014-11-07 15:20:58 +0100

    Added compressor unit test

---
+++ tests/phpbu/Backup/CompressorTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace phpbu\Backup;
+/**
+ * Compressor test
+ *
+ * @package    phpbu
+ * @subpackage tests
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  2014 Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.phpbu.de/
+ * @since      Class available since Release 1.0.0
+ */
+class CompressorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test gzip compressor
+     */
+    public function testGzip()
+    {
+        $gzip = Compressor::create('gzip');
+        $this->assertEquals('gz', $gzip->getSuffix());
+        $this->assertEquals('gzip', $gzip->getCommand());
+    }
+
+    /**
+     * Test bzip2 compressor
+     */
+    public function testBzip2()
+    {
+        $gzip = Compressor::create('bzip2');
+        $this->assertEquals('bz2', $gzip->getSuffix());
+        $this->assertEquals('bzip2', $gzip->getCommand());
+    }
+
+    /**
+     * Test zip compressor
+     */
+    public function testZip()
+    {
+        $gzip = Compressor::create('zip');
+        $this->assertEquals('zip', $gzip->getSuffix());
+        $this->assertEquals('zip', $gzip->getCommand());
+    }
+
+    /**
+     * Tets compressor with path to binary
+     */
+    public function testGzipWithPath()
+    {
+        $gzip = Compressor::create('/usr/local/bin/gzip');
+        $this->assertEquals('gz', $gzip->getSuffix());
+        $this->assertEquals('/usr/local/bin/gzip', $gzip->getCommand());
+    }
+}
