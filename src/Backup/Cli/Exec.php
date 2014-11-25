cb1460cbfe9730657a00b016efe96cb56372fd03
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2014-11-25 16:17:12 +0100

    Moved 'PIPESTATUS' so it's only added when it's needed

--- src/Backup/Cli/Exec.php
+++ src/Backup/Cli/Exec.php
@@ -91,9 +91,8 @@ class Exec
 
         if ($this->compressOutput) {
             if ($this->target->shouldBeCompressed()) {
-                $cmd    .= ' 2>&1 | ' . $this->target->getCompressor()->getCommand();
+                $cmd .= ' 2>&1 | ' . $this->target->getCompressor()->getCommand() . ' > ' . (string) $this->target . ' ; test ${PIPESTATUS[0]} -eq 0';
             }
-            $cmd .= ' > ' . (string) $this->target . ' ; test ${PIPESTATUS[0]} -eq 0';
         }
 
         return $cmd;
