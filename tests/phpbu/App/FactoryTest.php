ec1238870712dcff8ae0c5ed19185b4348794241
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2015-02-28 18:25:46 +0100

    Added 'Factory' unit tests

---
+++ tests/phpbu/App/FactoryTest.php
@@ -0,0 +1,152 @@
+<?php
+namespace phpbu\App;
+
+use phpbu\Backup\Check;
+use phpbu\Backup\Collector;
+use phpbu\Backup\Target;
+
+/**
+ * Args parser test
+ *
+ * @package    phpbu
+ * @subpackage tests
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.phpbu.de/
+ * @since      Class available since Release 1.0.5
+ */
+class FactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests Factory::createSource
+     */
+    public function testCreateSource()
+    {
+        $source = Factory::createSource('tar', array());
+        
+        $this->assertEquals(get_class($source), 'phpbu\\Backup\\Source\\Tar', 'classes should match');
+    }
+
+    /**
+     * Tests Factory::createLogger
+     */
+    public function testCreateLogger()
+    {
+        $logger = Factory::createLogger('mail', array('recipients' => 'no-reply@phpbu.de'));
+
+        $this->assertEquals(get_class($logger), 'phpbu\\Log\\Mail', 'classes should match');
+    }
+
+    /**
+     * Tests Factory::createCheck
+     */
+    public function testCreateCheck()
+    {
+        $check = Factory::createCheck('sizemin');
+
+        $this->assertEquals(get_class($check), 'phpbu\\Backup\\Check\\SizeMin', 'classes should match');
+    }
+
+    /**
+     * Tests Factory::createSync
+     */
+    public function testCreateSync()
+    {
+        $sync = Factory::createSync('Rsync', array('args' => 'foo'));
+
+        $this->assertEquals(get_class($sync), 'phpbu\\Backup\\Sync\\Rsync', 'classes should match');
+    }
+
+    /**
+     * Tests Factory::createCleaner
+     */
+    public function testCreateCleaner()
+    {
+        $sync = Factory::createCleaner('Capacity', array('size' => '10M'));
+
+        $this->assertEquals(get_class($sync), 'phpbu\\Backup\\Cleaner\\Capacity', 'classes should match');
+    }
+
+    /**
+     * Tests Factory::createType
+     * 
+     * @expectedException \phpbu\App\Exception
+     */
+    public function testCreateUnknown()
+    {
+        $failed = Factory::create('sync', 'Unknown', array('foo' => 'bar'));
+
+        $this->assertFalse(true, 'exception should be thrown');
+    }
+
+    /**
+     * Tests Factory::register
+     */
+    public function testRegisterCheckOk()
+    {
+        Factory::register('check', 'dummy', '\\phpbu\\App\\phpbuAppFactoryTestCheck');
+        
+        $dummy = Factory::create('check', 'dummy');
+
+        $this->assertEquals(get_class($dummy), 'phpbu\\App\\phpbuAppFactoryTestCheck', 'Factory should create dummy object');
+    }
+
+    /**
+     * Tests Factory::register
+     *
+     * @expectedException \phpbu\App\Exception
+     */
+    public function testRegisterInvalidType()
+    {
+        Factory::register('invalid', 'dummy', '\\phpbu\\App\\phpbuAppFactoryTestCheck');
+
+        $this->assertFalse(true, 'Exception should be thrown');
+    }
+
+    /**
+     * Tests Factory::register
+     * 
+     * @expectedException \phpbu\App\Exception 
+     */
+    public function testRegisterExistingCheck()
+    {
+        Factory::register('check', 'sizemin', '\\phpbu\\App\\phpbuAppFactoryTestCheck');
+
+        $this->assertFalse(true, 'Exception should be thrown');
+    }
+
+    /**
+     * Tests Factory::register
+     *
+     * @depends testRegisterExistingCheck
+     */
+    public function testRegisterExistingCheckForce()
+    {
+        Factory::register('check', 'sizemin', '\\phpbu\\App\\phpbuAppFactoryTestCheck', true);
+
+        $dummy = Factory::create('check', 'sizemin');
+
+        $this->assertEquals(get_class($dummy), 'phpbu\\App\\phpbuAppFactoryTestCheck', 'Factory should create dummy object');
+    }
+}
+
+/**
+ * Class phpbuAppFactoryTestObject
+ */
+class phpbuAppFactoryTestCheck implements Check {
+
+    /**
+     * Checks the created backup.
+     *
+     * @param  \phpbu\Backup\Target $target
+     * @param  string $value
+     * @param  \phpbu\Backup\Collector
+     * @param  \phpbu\App\Result
+     * @return boolean
+     */
+    public function pass(Target $target, $value, Collector $collector, Result $result)
+    {
+        // do something fooish
+    }
+}
