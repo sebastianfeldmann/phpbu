2b41e746047da34c55f2ec1bf4c0db8bad9f9900
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2015-03-08 14:12:06 +0100

    Added experimental 'MongoDB' backup and unit tests

---
+++ src/Backup/Source/Mongodump.php
@@ -0,0 +1,246 @@
+<?php
+namespace phpbu\App\Backup\Source;
+
+use phpbu\App\Exception;
+use phpbu\App\Result;
+use phpbu\App\Backup\Cli\Cmd;
+use phpbu\App\Backup\Cli\Exec;
+use phpbu\App\Backup\Source;
+use phpbu\App\Backup\Target;
+use phpbu\App\Util;
+
+/**
+ * Mongodump source class.
+ *
+ * @package    phpbu
+ * @subpackage Backup
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://phpbu.de/
+ * @since      Class available since Release 1.1.6
+ */
+class Mongodump extends Cli implements Source
+{
+    /**
+     * Show stdErr
+     *
+     * @var boolean
+     */
+    private $showStdErr;
+
+    /**
+     * Use IPv6
+     * --ipv6
+     *
+     * @var boolean
+     */
+    private $useIPv6;
+
+    /**
+     * Host to connect to
+     * --host <hostname:port>
+     *
+     * @var string
+     */
+    private $host;
+
+    /**
+     * User to connect with
+     * --user <username>
+     *
+     * @var string
+     */
+    private $user;
+
+    /**
+     * Password to authenticate with
+     * --password <password>
+     *
+     * @var string
+     */
+    private $password;
+
+    /**
+     * Database to use for authentication
+     * --authenticationDatabase <dbname>
+     *
+     * @var string
+     */
+    private $authenticationDatabase;
+
+    /**
+     * List of databases to backup
+     * --db <database>
+     *
+     * @var array
+     */
+    private $databases;
+
+    /**
+     * List of collections to backup
+     * --collection <collection>
+     *
+     * @var array
+     */
+    private $collections;
+
+    /**
+     * List of collections to ignore
+     * --excludeCollections array of strings
+     *
+     * @var array
+     */
+    private $excludeCollections;
+
+    /**
+     * List of prefixes to exclude collections
+     * --excludeCollectionWithPrefix array of strings
+     *
+     * @var array
+     */
+    private $excludeCollectionsWithPrefix;
+
+    /**
+     * Use php to validate the MongoDB connection
+     *
+     * @var boolean
+     */
+    private $validateConnection;
+
+    /**
+     * (No PHPDoc)
+     *
+     * @see    \phpbu\App\Backup\Source
+     * @param  array $conf
+     * @throws \phpbu\App\Exception
+     */
+    public function setup(array $conf = array())
+    {
+        $this->setupMongodump($conf);
+        $this->setupSourceData($conf);
+
+        // credentials
+        $this->host                   = Util\Arr::getValue($conf, 'host');
+        $this->user                   = Util\Arr::getValue($conf, 'user');
+        $this->password               = Util\Arr::getValue($conf, 'password');
+        $this->authenticationDatabase = Util\Arr::getValue($conf, 'authenticationDatabase');
+
+        // config & validation
+        $this->useIPv6            = Util\String::toBoolean(Util\Arr::getValue($conf, 'ipv6', ''), false);
+        $this->showStdErr         = Util\String::toBoolean(Util\Arr::getValue($conf, 'showStdErr', ''), false);
+        $this->validateConnection = Util\String::toBoolean(Util\Arr::getValue($conf, 'validateConnection', ''), false);
+    }
+
+    /**
+     * Search for Mongodump command.
+     *
+     * @param array $conf
+     */
+    protected function setupMongodump(array $conf)
+    {
+        if (empty($this->binary)) {
+            $this->binary = Util\Cli::detectCmdLocation('mongodump', Util\Arr::getValue($conf, 'pathToMongodump'));
+        }
+    }
+
+    /**
+     * Fetch databases and collections to backup.
+     *
+     * @param array $conf
+     */
+    protected function setupSourceData(array $conf)
+    {
+        $this->databases                    = Util\String::toList(Util\Arr::getValue($conf, 'databases'));
+        $this->collections                  = Util\String::toList(Util\Arr::getValue($conf, 'collections'));
+        $this->excludeCollections           = Util\String::toList(Util\Arr::getValue($conf, 'excludeCollections'));
+        $this->excludeCollectionsWithPrefix = Util\String::toList(Util\Arr::getValue($conf, 'excludeCollectionsWithPrefix'));
+    }
+
+    /**
+     * (non-PHPDoc)
+     *
+     * @see    \phpbu\App\Backup\Source
+     * @param  \phpbu\App\Backup\Target $target
+     * @param  \phpbu\App\Result        $result
+     * @return \phpbu\App\Result
+     * @throws \phpbu\App\Exception
+     */
+    public function backup(Target $target, Result $result)
+    {
+        if ($this->validateConnection) {
+            $this->checkConnection($this->host, $this->user, $this->password, $this->databases);
+        }
+
+        $exec      = $this->getExec($target);
+        $cmdResult = $this->execute($exec, $target);
+
+        $result->debug($cmdResult->getCmd());
+
+        if (!$cmdResult->wasSuccessful()) {
+            throw new Exception('Mongodump failed');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Create the Exec to run the Mongodump command
+     *
+     * @param  \phpbu\App\Backup\Target $target
+     * @return \phpbu\App\Backup\Cli\Exec
+     */
+    public function getExec(Target $target)
+    {
+        if (null == $this->exec) {
+            $dump       = dirname($target->getPath()) . '/dump';
+            $this->exec = new Exec();
+            $cmd        = new Cmd($this->binary);
+            $this->exec->addCommand($cmd);
+
+            // no std error unless it is activated
+            if (!$this->showStdErr) {
+                $cmd->silence();
+                // i kill you
+            }
+
+            $cmd->addOption('--out', $dump, ' ');
+            $this->addOptionIfNotEmpty($cmd, '--ipv6', $this->useIPv6, false);
+            $this->addOptionIfNotEmpty($cmd, '--host', $this->host, true, ' ');
+            $this->addOptionIfNotEmpty($cmd, '--user', $this->user, true, ' ');
+            $this->addOptionIfNotEmpty($cmd, '--password', $this->password, true, ' ');
+            $this->addOptionIfNotEmpty($cmd, '--authenticationDatabase', $this->authenticationDatabase, true, ' ');
+
+            if (count($this->databases)) {
+                foreach ($this->databases as $db) {
+                    $cmd->addOption('--database', $db, ' ');
+                }
+            }
+
+            if (count($this->collections)) {
+                foreach ($this->collections as $col) {
+                    $cmd->addOption('--collection', $col, ' ');
+                }
+            }
+
+            $this->addOptionIfNotEmpty($cmd, '--excludeCollection', $this->excludeCollections);
+            $this->addOptionIfNotEmpty($cmd, '--excludeCollectionWithPrefix', $this->excludeCollectionsWithPrefix);
+        }
+
+        return $this->exec;
+    }
+
+    /**
+     * Test MongoDB connection.
+     *
+     * @param  string $host
+     * @param  string $user
+     * @param  string $password
+     * @param  array  $databases
+     * @throws \phpbu\App\Exception
+     */
+    public function checkConnection($host, $user, $password, array $databases = array())
+    {
+        // ToDo: implement mongo db connection validation
+    }
+}
---
+++ tests/phpbu/Backup/Source/MongodumpTest.php
@@ -0,0 +1,261 @@
+<?php
+namespace phpbu\App\Backup\Source;
+
+/**
+ * MongodumpTest
+ *
+ * @package    phpbu
+ * @subpackage tests
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.phpbu.de/
+ * @since      Class available since Release 1.1.6
+ */
+class MongodumpTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Mongodump
+     *
+     * @var \phpbu\App\Backup\Source\Mongodump
+     */
+    protected $mongodump;
+
+    /**
+     * Setup Mongodump
+     */
+    public function setUp()
+    {
+        $this->mongodump = new Mongodump();
+        $this->mongodump->setBinary('mongodump');
+    }
+
+    /**
+     * Clear Mongodump
+     */
+    public function tearDown()
+    {
+        $this->mongodump = null;
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testDefault()
+    {
+        $this->mongodump->setup(array());
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testShowStdErr()
+    {
+        $this->mongodump->setup(array('showStdErr' => 'true'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\'', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testUser()
+    {
+        $this->mongodump->setup(array('user' => 'root'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' --user \'root\' 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testPassword()
+    {
+        $this->mongodump->setup(array('password' => 'secret'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' --password \'secret\' 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testHost()
+    {
+        $this->mongodump->setup(array('host' => 'example.com'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' --host \'example.com\' 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testDatabases()
+    {
+        $this->mongodump->setup(array('databases' => 'db1,db2'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' --database \'db1\' --database \'db2\' 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testCollections()
+    {
+        $this->mongodump->setup(array('collections' => 'collection1,collection2'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' --collection \'collection1\' --collection \'collection2\' 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testIPv6()
+    {
+        $this->mongodump->setup(array('ipv6' => 'true'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' --ipv6 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testExcludeCollections()
+    {
+        $this->mongodump->setup(array('excludeCollections' => 'col1,col2'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' --excludeCollection \'col1\' \'col2\' 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::getExec
+     */
+    public function testExcludeCollectionsWithPrefix()
+    {
+        $this->mongodump->setup(array('excludeCollectionsWithPrefix' => 'pre1,pre2'));
+        /** @var \phpbu\App\Backup\Cli\Exec $exec */
+        $exec = $this->mongodump->getExec($this->getTargetMock());
+        $cmd  = (string) $exec->getExec();
+
+        $this->assertEquals('mongodump --out \'./dump\' --excludeCollectionWithPrefix \'pre1\' \'pre2\' 2> /dev/null', $cmd);
+    }
+
+    /**
+     * Tests Mongodump::checkConnection
+     */
+    public function testCheckConnection()
+    {
+        $this->mongodump->checkConnection('localhost', 'user', 'password');
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Tests Mongodump::backup
+     */
+    public function testBackupOk()
+    {
+        $target    = $this->getTargetMock();
+        $cliResult = $this->getCliResultMock(0);
+        $appResult = $this->getMockBuilder('\\phpbu\\App\\Result')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+        $exec      = $this->getMockBuilder('\\phpbu\\App\\Backup\\Cli\\Exec')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+
+        $appResult->expects($this->once())->method('debug');
+        $exec->expects($this->once())->method('execute')->willReturn($cliResult);
+
+        $this->mongodump->setup(array());
+        $this->mongodump->setExec($exec);
+        $this->mongodump->backup($target, $appResult);
+    }
+
+    /**
+     * Tests Mongodump::backup
+     *
+     * @expectedException \phpbu\App\Exception
+     */
+    public function testBackupFail()
+    {
+        $target    = $this->getTargetMock();
+        $cliResult = $this->getCliResultMock(1);
+        $appResult = $this->getMockBuilder('\\phpbu\\App\\Result')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+        $exec      = $this->getMockBuilder('\\phpbu\\App\\Backup\\Cli\\Exec')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+
+        $appResult->expects($this->once())->method('debug');
+        $exec->expects($this->once())->method('execute')->willReturn($cliResult);
+
+        $this->mongodump->setup(array());
+        $this->mongodump->setExec($exec);
+        $this->mongodump->backup($target, $appResult);
+    }
+
+    /**
+     * Create Cli\Result mock.
+     *
+     * @param  integer $code
+     * @return \phpbu\App\Backup\Cli\Result
+     */
+    protected function getCliResultMock($code)
+    {
+        $cliResult = $this->getMockBuilder('\\phpbu\\App\\Backup\\Cli\\Result')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+
+        $cliResult->method('getCmd')->willReturn('mongodump');
+        $cliResult->method('getCode')->willReturn($code);
+        $cliResult->method('getOutput')->willReturn(array());
+        $cliResult->method('wasSuccessful')->willReturn($code == 0);
+
+        return $cliResult;
+    }
+
+    /**
+     * Create Target mock.
+     *
+     * @return \phpbu\App\Backup\Target
+     */
+    protected function getTargetMock()
+    {
+        $target = $this->getMockBuilder('\\phpbu\\App\\Backup\\Target')
+                       ->disableOriginalConstructor()
+                       ->getMock();
+        $target->method('getPath')->willReturn('.');
+        $target->method('fileExists')->willReturn(false);
+
+        return $target;
+    }
+}
