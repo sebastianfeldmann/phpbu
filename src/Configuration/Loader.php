2c77061a35a42500d228a0bc6fef674eeda5b3df
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2015-04-02 20:31:36 +0200

    More PHPDoc and removed unused Option class

--- src/Configuration/Loader.php
+++ src/Configuration/Loader.php
@@ -1,6 +1,17 @@
 <?php
 namespace phpbu\App\Configuration;
 
+/**
+ * Configuration loader interface.
+ *
+ * @package    phpbu
+ * @subpackage App
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://phpbu.de/
+ * @since      Class available since Release 2.0.0
+ */
 interface Loader
 {
     /**
--- src/Configuration/Logger.php
+++ src/Configuration/Logger.php
@@ -1,10 +1,32 @@
 <?php
 namespace phpbu\App\Configuration;
 
+/**
+ * Logger configuration.
+ *
+ * @package    phpbu
+ * @subpackage App
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://phpbu.de/
+ * @since      Class available since Release 2.0.0
+ */
 class Logger extends Optionized
 {
+    /**
+     * Logger type.
+     *
+     * @var string
+     */
     public $type;
 
+    /**
+     * Constructor.
+     *
+     * @param string $type
+     * @param array  $options
+     */
     public function __construct($type, $options = array())
     {
         $this->type = $type;
