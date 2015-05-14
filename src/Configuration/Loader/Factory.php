ae95320f89f24a9b84086834cef518f314d83e6e
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2015-05-14 22:48:15 +0200

    Added 'Factory' to create 'Loader' based on file type

---
+++ src/Configuration/Loader/Factory.php
@@ -0,0 +1,50 @@
+<?php
+namespace phpbu\App\Configuration\Loader;
+
+/**
+ *
+ * Factory class for file based Configuration Loader.
+ *
+ * @package    phpbu
+ * @subpackage App
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://phpbu.de/
+ * @since      Class available since Release 2.1.2
+ */
+abstract class Factory
+{
+    /**
+     * Default loader to return if none could be detected.
+     *
+     * @var string
+     */
+    const DEFAULT_LOADER = 'Xml';
+
+    /**
+     * File type to loader class map.
+     *
+     * @var array
+     */
+    private static $extToLoaderMap = array(
+        'xml'  => 'Xml',
+        'json' => 'Json',
+    );
+
+    /**
+     * Create a Configuration Loader based on the file to load.
+     *
+     * @param  string $filename
+     * @throws \phpbu\App\Exception
+     * @return \phpbu\App\Configuration\Loader
+     */
+    public static function createLoader($filename)
+    {
+        $ext   = pathinfo($filename, PATHINFO_EXTENSION);
+        $type  = isset(self::$extToLoaderMap[$ext]) ? self::$extToLoaderMap[$ext] : self::DEFAULT_LOADER;
+        $class = '\\phpbu\\App\\Configuration\\Loader\\' . $type;
+
+        return new $class($filename);
+    }
+}
