15e3bd144e8ab4a054e22ec432cbcf5f34bbf39c
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2015-08-30 20:50:01 +0200

    Added OpenSSL Crypter

---
+++ src/Backup/Crypter/OpenSSL.php
@@ -0,0 +1,144 @@
+<?php
+namespace phpbu\App\Backup\Crypter;
+
+use phpbu\App\Backup\Crypter;
+use phpbu\App\Backup\Target;
+use phpbu\App\Cli\Executable;
+use phpbu\App\Result;
+use phpbu\App\Util;
+
+/**
+ * OpenSSL crypter class.
+ *
+ * @package    phpbu
+ * @subpackage Backup
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://phpbu.de/
+ * @since      Class available since Release 2.1.6
+ */
+class OpenSSL extends Key implements Crypter
+{
+    /**
+     * Path to mcrypt command.
+     *
+     * @var string
+     */
+    private $pathToOpenSSL;
+
+    /**
+     * @var boolean
+     */
+    private $showStdErr;
+
+    /**
+     * Key file
+     *
+     * @var string
+     */
+    private $certFile;
+
+    /**
+     * Algorithm to use
+     *
+     * @var string
+     */
+    private $algorithm;
+
+    /**
+     * Password to use
+     *
+     * @var string
+     */
+    private $password;
+
+    /**
+     * Keep the not encrypted file
+     *
+     * @var boolean
+     */
+    private $keepUncrypted;
+
+    /**
+     * Setup.
+     *
+     * @see    \phpbu\App\Backup\Crypter
+     * @param  array $options
+     * @throws Exception
+     */
+    public function setup(array $options = array())
+    {
+        if (!Util\Arr::isSetAndNotEmptyString($options, 'algorithm')) {
+            throw new Exception('openssl expects \'algorithm\'');
+        }
+        if (!Util\Arr::isSetAndNotEmptyString($options, 'password')
+         && !Util\Arr::isSetAndNotEmptyString($options, 'certFile')) {
+            throw new Exception('openssl expects \'key\' or \'password\'');
+        }
+
+        $this->pathToOpenSSL = Util\Arr::getValue($options, 'pathToOpenSSL');
+        $this->showStdErr    = Util\Str::toBoolean(Util\Arr::getValue($options, 'showStdErr', ''), false);
+        $this->keepUncrypted = Util\Str::toBoolean(Util\Arr::getValue($options, 'keepUncrypted', ''), false);
+        $this->certFile      = $this->toAbsolutePath(Util\Arr::getValue($options, 'certFile'));
+        $this->algorithm     = Util\Arr::getValue($options, 'algorithm');
+        $this->password      = Util\Arr::getValue($options, 'password');
+    }
+
+    /**
+     * (non-PHPDoc)
+     *
+     * @see    \phpbu\App\Backup\Crypter
+     * @param  \phpbu\App\Backup\Target $target
+     * @param  \phpbu\App\Result        $result
+     * @throws Exception
+     */
+    public function crypt(Target $target, Result $result)
+    {
+        $openssl = $this->execute($target);
+
+        $result->debug('openssl:' . $openssl->getCmd());
+
+        if (!$openssl->wasSuccessful()) {
+            throw new Exception('openssl failed:' . PHP_EOL . $openssl->getOutputAsString());
+        }
+    }
+
+    /**
+     * (non-PHPDoc)
+     *
+     * @see    \phpbu\App\Backup\Crypter
+     * @return string
+     */
+    public function getSuffix()
+    {
+        return 'enc';
+    }
+
+    /**
+     * Create the Exec to run the 'mcrypt' command.
+     *
+     * @param  \phpbu\App\Backup\Target $target
+     * @return \phpbu\App\Cli\Executable
+     */
+    public function getExecutable(Target $target)
+    {
+        if (null == $this->executable) {
+            $this->executable = new Executable\OpenSSL($this->pathToOpenSSL);
+            $this->executable->encryptFile($target->getPathname());
+
+            // use key or password to encrypt
+            if (!empty($this->certFile)) {
+                $this->executable->useSSLCert($this->certFile);
+            } else {
+                $this->executable->usePassword($this->password)
+                                 ->encodeBase64(true);
+            }
+            $this->executable->useAlgorithm($this->algorithm)
+                             ->deleteUncrypted(!$this->keepUncrypted)
+                             ->showStdErr($this->showStdErr);
+        }
+
+        return $this->executable;
+    }
+}
---
+++ tests/phpbu/Backup/Crypter/OpenSSLTest.php
@@ -0,0 +1,157 @@
+<?php
+namespace phpbu\App\Backup\Crypter;
+
+use phpbu\App\Backup\CliTest;
+use phpbu\App\Util\Cli;
+
+/**
+ * OpenSSLTest
+ *
+ * @package    phpbu
+ * @subpackage tests
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.phpbu.de/
+ * @since      Class available since Release 2.1.6
+ */
+class OpenSSLTest extends CliTest
+{
+    /**
+     * @var OpenSSL
+     */
+    protected $openSSL;
+
+    /**
+     * Setup OpenSSL Crypt
+     */
+    public function setUp()
+    {
+        $this->openSSL = new OpenSSL();
+    }
+
+    /**
+     * Clear OpenSSL Crypt
+     */
+    public function tearDown()
+    {
+        $this->openSSL = null;
+    }
+
+    /**
+     * Tests OpenSSL::setUp
+     */
+    public function testSetUpOk()
+    {
+        $this->openSSL->setup(array('password' => 'fooBarBaz', 'algorithm' => 'aes-256-cbc'));
+
+        $this->assertTrue(true, 'no exception should occur');
+    }
+
+    /**
+     * Tests OpenSSL::setUp
+     *
+     * @expectedException \phpbu\App\Backup\Crypter\Exception
+     */
+    public function testSetUpNoCertOrPassword()
+    {
+        $this->openSSL->setup(array('algorithm' => 'aes-256-cbc'));
+    }
+
+    /**
+     * Tests OpenSSL::setUp
+     *
+     * @expectedException \phpbu\App\Backup\Crypter\Exception
+     */
+    public function testSetUpNoAlgorithm()
+    {
+        $this->openSSL->setup(array('password' => 'fooBarBaz'));
+    }
+
+    /**
+     * Tests OpenSSL::getExecutable
+     */
+    public function testPasswordAndAlgorithm()
+    {
+        $expected = 'openssl enc -e -a -aes-256-cbc -pass \'pass:fooBarBaz\' '
+                  . '-in \'/foo/bar.txt\' -out \'/foo/bar.txt.enc\' 2> /dev/null '
+                  . '&& rm \'/foo/bar.txt\' 2> /dev/null';
+        $target   = $this->getTargetMock('/foo/bar.txt');
+        $path     = $this->getBinDir();
+        $this->openSSL->setup(array('pathToOpenSSL' => $path, 'password' => 'fooBarBaz', 'algorithm' => 'aes-256-cbc'));
+
+        $executable = $this->openSSL->getExecutable($target);
+
+        $this->assertEquals('(' . $path . '/' . $expected . ')', $executable->getCommandLine());
+    }
+
+    /**
+     * Tests OpenSSL::getExecutable
+     */
+    public function testCertFile()
+    {
+        Cli::registerBase('configuration', '/foo');
+
+        $expected = 'openssl smime -encrypt -aes256 -binary -in \'/foo/bar.txt\' '
+                  . '-out \'/foo/bar.txt.enc\' -outform DER \'/foo/my.pem\' 2> /dev/null '
+                  . '&& rm \'/foo/bar.txt\' 2> /dev/null';
+        $target   = $this->getTargetMock('/foo/bar.txt');
+        $path     = $this->getBinDir();
+        $this->openSSL->setup(array('pathToOpenSSL' => $path, 'certFile' => '/foo/my.pem', 'algorithm' => 'aes256'));
+
+        $executable = $this->openSSL->getExecutable($target);
+
+        $this->assertEquals('(' . $path . '/' . $expected . ')', $executable->getCommandLine());
+    }
+
+    /**
+     * Tests OpenSSL::crypt
+     */
+    public function testCryptOk()
+    {
+        $target    = $this->getTargetMock();
+        $cliResult = $this->getCliResultMock(0, 'openssl');
+        $appResult = $this->getAppResultMock();
+        $exec      = $this->getMockBuilder('\\phpbu\\App\\Cli\\Executable\\OpenSSL')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+
+        $exec->expects($this->once())->method('run')->willReturn($cliResult);
+        $appResult->expects($this->once())->method('debug');
+
+        $this->openSSL->setExecutable($exec);
+        $this->openSSL->crypt($target, $appResult);
+    }
+
+    /**
+     * Tests OpenSSL::crypt
+     *
+     * @expectedException \phpbu\App\Backup\Crypter\Exception
+     */
+    public function testCryptFail()
+    {
+        $target    = $this->getTargetMock();
+        $cliResult = $this->getCliResultMock(1, 'openSSL');
+        $appResult = $this->getMockBuilder('\\phpbu\\App\\Result')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+        $exec      = $this->getMockBuilder('\\phpbu\\App\\Cli\\Executable\\OpenSSL')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+
+        $exec->expects($this->once())->method('run')->willReturn($cliResult);
+        $appResult->expects($this->once())->method('debug');
+
+        $this->openSSL->setExecutable($exec);
+        $this->openSSL->crypt($target, $appResult);
+    }
+
+    /**
+     * Tests OpenSSL::getSuffix
+     */
+    public function testGetSuffix()
+    {
+        $suffix = $this->openSSL->getSuffix();
+        $this->assertEquals('enc', $suffix);
+    }
+}
