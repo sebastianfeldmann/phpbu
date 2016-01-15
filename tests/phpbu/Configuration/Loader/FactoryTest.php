3b3cf81984b86e9ed7b336a375a04eeacb1946dc
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2016-01-15 13:54:38 +0100

    Add 'Loader\Factory' unit tests

---
+++ tests/phpbu/Configuration/Loader/FactoryTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace phpbu\App\Configuration\Loader;
+
+/**
+ * Loader Factory test
+ *
+ * @package    phpbu
+ * @subpackage tests
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    https://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link       http://www.phpbu.de/
+ * @since      Class available since Release 3.0.0
+ */
+class FactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests Factory::createLoader
+     */
+    public function testCreateLoader()
+    {
+        $file   = realpath(__DIR__ . '/../../../_files/conf/json/config-valid.json');
+        $loader = Factory::createLoader($file);
+
+        $this->assertTrue(is_a($loader, '\\phpbu\\App\\Configuration\\Loader\\Json'));
+    }
+}
