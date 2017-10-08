18dbb41539edb5853fdf8ce86f79b7a5221bb0af
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2017-10-08 10:52:04 +0200

    Enhance adapter value parsing

---
+++ tests/phpbu/Adapter/UtilTest.php
@@ -0,0 +1,101 @@
+<?php
+namespace phpbu\App\Adapter;
+
+/**
+ * Adapter Util test
+ *
+ * @package    phpbu
+ * @subpackage tests
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    https://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link       http://www.phpbu.de/
+ * @since      Class available since Release 5.0.7
+ */
+class UtilTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Tests Util::getAdapterReplacements
+     */
+    public function testGetAdapterValuesSingle()
+    {
+        $values = Util::getAdapterReplacements('adapter:foo:bar');
+
+        $this->assertEquals(1, count($values));
+        $this->assertEquals('adapter:foo:bar', $values[0]['search']);
+        $this->assertEquals('foo', $values[0]['adapter']);
+        $this->assertEquals('bar', $values[0]['path']);
+    }
+
+    /**
+     * Tests Util::getAdapterReplacements
+     */
+    public function testGetAdapterValuesSinglePrefixed()
+    {
+        $values = Util::getAdapterReplacements(':AdaPteR:foo:bar');
+
+        $this->assertEquals(1, count($values));
+        $this->assertEquals(':AdaPteR:foo:bar', $values[0]['search']);
+        $this->assertEquals('foo', $values[0]['adapter']);
+        $this->assertEquals('bar', $values[0]['path']);
+    }
+
+    /**
+     * Tests Util::getAdapterReplacements
+     */
+    public function testGetAdapterValuesSinglePostfixed()
+    {
+        $values = Util::getAdapterReplacements('adapter:foo:bar:');
+
+        $this->assertEquals(1, count($values));
+        $this->assertEquals('adapter:foo:bar:', $values[0]['search']);
+        $this->assertEquals('foo', $values[0]['adapter']);
+        $this->assertEquals('bar', $values[0]['path']);
+    }
+
+    /**
+     * Tests Util::getAdapterReplacements
+     */
+    public function testGetAdapterValuesSinglePreAndPostfixed()
+    {
+        $values = Util::getAdapterReplacements(':adapter:foo:bar:');
+
+        $this->assertEquals(1, count($values));
+        $this->assertEquals(':adapter:foo:bar:', $values[0]['search']);
+        $this->assertEquals('foo', $values[0]['adapter']);
+        $this->assertEquals('bar', $values[0]['path']);
+    }
+
+    /**
+     * Tests Util::getAdapterReplacements
+     */
+    public function testGetAdapterValuesMultiple()
+    {
+        $values = Util::getAdapterReplacements('adapter:foo:bar:/some/path/:adapter:fiz:baz');
+
+        $this->assertEquals(2, count($values));
+        $this->assertEquals('adapter:foo:bar:', $values[0]['search']);
+        $this->assertEquals('foo', $values[0]['adapter']);
+        $this->assertEquals('bar', $values[0]['path']);
+        $this->assertEquals(':adapter:fiz:baz', $values[1]['search']);
+        $this->assertEquals('fiz', $values[1]['adapter']);
+        $this->assertEquals('baz', $values[1]['path']);
+    }
+
+
+    /**
+     * Tests Util::getAdapterReplacements
+     */
+    public function testGetAdapterValuesMultipleInBetween()
+    {
+        $values = Util::getAdapterReplacements('/some/:adapter:foo:bar:/path/:adapter:fiz:baz:/end/');
+
+        $this->assertEquals(2, count($values));
+        $this->assertEquals(':adapter:foo:bar:', $values[0]['search']);
+        $this->assertEquals('foo', $values[0]['adapter']);
+        $this->assertEquals('bar', $values[0]['path']);
+        $this->assertEquals(':adapter:fiz:baz:', $values[1]['search']);
+        $this->assertEquals('fiz', $values[1]['adapter']);
+        $this->assertEquals('baz', $values[1]['path']);
+    }
+}
