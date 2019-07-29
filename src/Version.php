47791def1448e46c6e53cc20e4301255a3e0e378
Sebastian Feldmann <sebastian@phpbu.de>
2019-07-29 22:21:11 +0200

    Prepare version 5.2.9

--- src/Version.php
+++ src/Version.php
@@ -80,7 +80,7 @@ final class Version
     public static function id() : string
     {
         if (self::$version === null) {
-            $version = new self('5.2.8', dirname(dirname(__DIR__)));
+            $version = new self('5.2.9', dirname(dirname(__DIR__)));
             self::$version = $version->getVersionNumber();
         }
 
