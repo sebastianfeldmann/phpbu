a5bed0a6d615f1b3106e3afacc555301e4047bf2
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2019-08-25 20:04:04 +0200

    Add since class annotation

--- src/Log/File.php
+++ src/Log/File.php
@@ -12,7 +12,8 @@ use InvalidArgumentException;
  * @author     Sebastian Feldmann <sebastian@phpbu.de>
  * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
  * @license    https://opensource.org/licenses/MIT The MIT License (MIT)
- * @link       http://phpbu.de/
+ * @link       https://phpbu.de/
+ * @since      Class available since Release 1.0.0
  */
 class File
 {
