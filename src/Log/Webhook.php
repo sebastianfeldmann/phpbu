779afad7b9bb381869479904c0cdac021c24d65f
MoeBrowne <moebrowne@users.noreply.github.com>
2020-04-15 21:42:46 +0100

    Adds a PHP version check for filter options

--- src/Log/Webhook.php
+++ src/Log/Webhook.php
@@ -122,7 +122,14 @@ class Webhook implements Listener, Logger
             throw new Exception('no uri given');
         }
 
-        if (!filter_var($options['uri'], FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED | FILTER_FLAG_HOST_REQUIRED)) {
+        // PHP >7.2 deprecated the filter options and enabled them by default
+        if (version_compare(PHP_VERSION, '7.2.0', '<')) {
+            $filterOptions = FILTER_FLAG_SCHEME_REQUIRED | FILTER_FLAG_HOST_REQUIRED;
+        } else {
+            $filterOptions = null;
+        }
+
+        if (!filter_var($options['uri'], FILTER_VALIDATE_URL, $filterOptions)) {
             throw new Exception('webhook URI is invalid');
         }
 
