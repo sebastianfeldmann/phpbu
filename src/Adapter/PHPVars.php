30f1edddf88cedd8bc27e25eeee98bbbfbb32b2c
planetahuevo <planetahuevo@users.noreply.github.com>
2020-11-06 13:18:52 +0000

    New adapter to read php variables. Useful for reading WordPress config files.

---
+++ src/Adapter/PHPVars.php
@@ -0,0 +1,74 @@
+<?php
+namespace phpbu\App\Adapter;
+
+use phpbu\App\Adapter;
+use phpbu\App\Configuration;
+use phpbu\App\Exception;
+use phpbu\App\Util as AppUtil;
+
+/**
+ * PHPVars Adapter
+ *
+ * @package    phpbu
+ * @subpackage App
+ * @author     Sebastian Feldmann <sebastian@phpbu.de>
+ * @copyright  Sebastian Feldmann <sebastian@phpbu.de>
+ * @license    https://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link       http://phpbu.de/
+ * @since      Class available since Release 4.0.1
+ */
+class PHPVars implements Adapter
+{
+    /**
+     * Path to the config file
+     *
+     * @var string
+     */
+    private $file;
+
+    /**
+     * Configuration
+     *
+     * @var array
+     */
+    private $config;
+
+    /**
+     * Setup the adapter.
+     *
+     * @param  array $conf
+     * @return void
+     * @throws \phpbu\App\Exception
+     */
+    public function setup(array $conf)
+    {
+        $path       = AppUtil\Arr::getValue($conf, 'file', 'config.php');
+        $this->file = AppUtil\Path::toAbsolutePath($path, Configuration::getWorkingDirectory());
+        $this->load();
+    }
+
+    /**
+     * Load config file to local file.
+     *
+     * @throws \phpbu\App\Exception
+     */
+    private function load()
+    {
+        if (!file_exists($this->file)) {
+            throw new Exception('config file not found');
+        }
+        $this->config = require $this->file;
+    }
+
+    /**
+     * Return a value for a given path.
+     *
+     * @param  string $path
+     * @return string
+     * @throws \phpbu\App\Exception
+     */
+    public function getValue(string $path) : string
+    {
+        return (string) constant($path);
+    }
+}
