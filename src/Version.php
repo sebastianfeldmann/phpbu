6bb3962ecca07a33fb032a181d6a2b55419ba6b7
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-12-21 12:11:19 +0100

    Prepare version 6.0.21

--- src/Version.php
+++ src/Version.php
@@ -82,7 +82,7 @@ final class Version
     public static function id() : string
     {
         if (self::$version === null) {
-            $version = new self('6.0.20', dirname(dirname(__DIR__)));
+            $version = new self('6.0.21', dirname(dirname(__DIR__)));
             self::$version = $version->getVersionNumber();
         }
 
